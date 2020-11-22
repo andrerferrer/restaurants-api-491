cd11ea30ae4c45c358dcb9d39ac60e1f8068cc23
andre ferrer <andrerferrer@hotmail.com>
2020-11-22 13:56:02 -0300

    Add seeds to boilerplate to save time

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create! email: 'player@one.com', password: 123456
+User.create! email: 'player@two.com', password: 123456
+User.create! email: 'player@three.com', password: 123456
+
+9.times do |x|
+  rest = Restaurant.create! name: "Restaurant #{x}", address: "Street no #{x}", user: User.all.sample
+  3.times do
+    user = User.all.sample
+    Comment.create! content: "A comment from #{user.email}" ,restaurant: rest, user: user
+  end
+end
